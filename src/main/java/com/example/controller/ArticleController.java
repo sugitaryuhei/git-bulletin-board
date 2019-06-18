8ca107f17072f1c0849f888c8614b88d3b283e24
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-06-18 16:03:59 +0900

    Articleの作成

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,5 +1,131 @@
 package com.example.controller;
 
-public class ArticleController {
+import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.domain.Comment;
+import com.example.form.ArticleForm;
+import com.example.form.CommentForm;
+import com.example.repository.ArticleRepository;
+import com.example.repository.CommentRepository;
+
+/**
+ * アプリケーションを動かすためのコントローラー.
+ * 
+ * @author ryuheisugita
+ */
+@Controller
+@Transactional
+@RequestMapping("/article")
+public class ArticleController {
+	
+	@ModelAttribute
+	public ArticleForm SetUpArticleForm() {
+		return new ArticleForm();
+	}
+	
+	@ModelAttribute
+	public CommentForm SetUpCommentForm() {
+		return new CommentForm();
+	}
+	
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@Autowired
+	private CommentRepository commentRepository;
+
+	/**
+	 * 2つのSQL文で掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */
+//	@RequestMapping("")
+//	public String index(Model model) {
+//		List<Article> aricleList = articleRepository.findAllExcludeComment();
+//		for(Article article:aricleList) {
+//			List<Comment> commentList = commentRepository.findByArticleId(article.getId());
+//			article.setCommentList(commentList);
+//		}
+//		model.addAttribute("articleList", aricleList);
+//		return "article";
+//	}
+	
+	@RequestMapping("maintenance")
+	public String maintenance() {
+		return "error";
+	}
+	
+	/**
+	 * １つのSQL文で掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("")
+	public String index2(Model model) {
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList", articleList);
+		return "article";
+	}
+	
+	/**
+	 * 記事を投稿して、掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("/insert-article")
+	public String insertArticle(@Validated ArticleForm form, BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			return index(model);
+		}
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		articleRepository.insert(article);
+		return "redirect:/article";
+	}
+
+	/**
+	 * コメントを追加して、掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */	
+	@RequestMapping("/insert-comment")
+	public String insertComment(@Validated CommentForm form,
+			                                        BindingResult result,
+			                                        Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("selectedId", Integer.parseInt(form.getArticleId()));
+			return index2(model);
+		}
+		Comment comment = new Comment();
+		BeanUtils.copyProperties(form, comment);
+		comment.setArticleId(Integer.parseInt(form.getArticleId()));
+		commentRepository.insert(comment);
+		return "redirect:/article";
+	}
+	
+	/**
+	 * 記事を削除して、掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */	
+	@RequestMapping("/delete-article")
+	public String deleteArticle(int id) {
+		System.out.println(id);
+		articleRepository.deleteById1TimeSQL(id);
+//		commentRepository.delete(id);
+//		articleRepository.deleteById(id);
+		return "redirect:/article";
+	}
+	
 }
--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,5 +1,182 @@
 package com.example.repository;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+import com.example.domain.Comment;
+
+/**
+ * ariclesテーブルを操作するリポジトリ.
+ * 
+ * @author ryuheisugita
+ */
+@Repository
 public class ArticleRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private String tableName = "articles";
+
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+
+//	private static final RowMapper<Article> ARTICLE_ROW_MAPPER2 = (rs, i) -> {
+//		Article article = new Article();
+//
+//		article.setId(rs.getInt("id"));
+//		article.setName(rs.getString("name"));
+//		article.setContent(rs.getString("content"));
+//		Comment comment = new Comment();
+//		comment.setId(rs.getInt("c_id"));
+//		comment.setName(rs.getString("c_name"));
+//		comment.setContent(rs.getString("c_content"));
+//		comment.setArticleId(rs.getInt("id"));
+//		List<Comment> commentList = new ArrayList<>();
+//		commentList.add(comment);
+//		if (comment.getName() == null) {
+//			commentList = null;
+//		}
+//		article.setCommentList(commentList);
+//		return article;
+//	};
+
+	private static final ResultSetExtractor<List<Article>> ARTICLE_RESULT_SET_EXTRACTOR = (rs) -> {
+		List<Article> articleList = new LinkedList<>();
+		int beforeArticleId = 0;
+
+		List<Comment> commentList = null;
+
+		while (rs.next()) {
+
+			if (beforeArticleId != rs.getInt("id")) {
+				Article article = new Article();
+				article.setName(rs.getString("name"));
+				article.setContent(rs.getString("content"));
+				article.setId(rs.getInt("id"));
+				articleList.add(article);
+				commentList = new LinkedList<>();
+				article.setCommentList(commentList);
+			}
+
+			if (rs.getInt("c_id") != 0) {
+				Comment comment = new Comment();
+				comment.setId(rs.getInt("c_id"));
+				comment.setName(rs.getString("c_name"));
+				comment.setContent(rs.getString("c_content"));
+				comment.setArticleId(rs.getInt("id"));
+				commentList.add(comment);
+			}
+			beforeArticleId = rs.getInt("id");
+		}
+		return articleList;
+	};
+
+	/**
+	 * 投稿されているすべての記事を表示する(コメントを含まない).
+	 * 
+	 * @return 投稿されているすべての記事
+	 */
+	public List<Article> findAllExcludeComment() {
+		String sql = "select id,name,content from " + tableName + " order by id desc";
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		return articleList;
+	}
+
+	/**
+	 * 投稿されているすべての記事を表示する(一つのSQL文で).
+	 * 
+	 * @return 投稿されているすべての記事
+	 */
+//	public List<Article> findAll() {
+//		// ArticleのcommentListに一つずつcomment を入れて、List<Article>を取得
+//		String sql = "select a.id as id,a.name as name,a.content as content,c.id as c_id,c.name as c_name,c.content as c_content from "
+//				+ tableName + " a full join comments c on a.id=c.article_id order by a.id desc,c.id desc";
+//		List<Article> articleListNotCommentList = template.query(sql, ARTICLE_ROW_MAPPER2);
+//		// 記事IDをLinkedHashMapのキーとして、同じキーのcommentListをまとめる
+//		Map<Integer, Article> articleMap = new LinkedHashMap<>();
+//		for (Article article : articleListNotCommentList) {
+//			if (articleMap.get(article.getId()) == null) {
+//				articleMap.put(article.getId(), article);
+//			} else {
+//				List<Comment> commentList = articleMap.get(article.getId()).getCommentList();
+//				commentList.add(article.getCommentList().get(0));
+//				article.setCommentList(commentList);
+//				articleMap.put(article.getId(), article);
+//			}
+//		}
+//		// LinkedHashMapからarticleListを生成
+//		Set<Integer> set = articleMap.keySet();
+//		List<Article> articleList = new LinkedList<Article>();
+//		for (Integer id : set) {
+//			articleList.add(articleMap.get(id));
+//		}
+//		return articleList;
+//	}
+
+	/**
+	 * 投稿されているすべての記事を表示する(一つのSQL文で).
+	 * 
+	 * @return 投稿されているすべての記事
+	 */
+	public List<Article> findAll() {
+		// ArticleのcommentListに一つずつcomment を入れて、List<Article>を取得
+		String sql = "select a.id as id,a.name as name,a.content as content,c.id as c_id,c.name as c_name,c.content as c_content from "
+				+ tableName + " a full join comments c on a.id=c.article_id order by a.id desc,c.id desc";
+
+		return template.query(sql, ARTICLE_RESULT_SET_EXTRACTOR);
+	}
+
+	/**
+	 * 投稿された記事をarticleテーブルに書き込む.
+	 * 
+	 * @param article 投稿記事
+	 */
+	public void insert(Article article) {
+		String sql = "insert into " + tableName + " (name,content) values(:name,:content)";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(article);
+		template.update(sql, param);
+	}
+
+	/**
+	 * 渡された記事IDの投稿をarticlesテーブルから削除する.
+	 * 
+	 * @param id 記事ID
+	 */
+	public void deleteById(int id) {
+		String sql = "delete from " + tableName + " where id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		template.update(sql, param);
+	}
+	
+	/**
+	 * 一つのSQL文に渡された記事IDの投稿をarticlesテーブルから削除する.
+	 * 
+	 * @param id 記事ID
+	 */
+	public void deleteById1TimeSQL(int id) {
+		String sql = "WITH deleted AS " + 
+				"(DELETE FROM "+tableName+" WHERE id =:id" + 
+				" RETURNING id)" + 
+				" DELETE FROM comments " + 
+				" WHERE article_id IN (SELECT id FROM deleted)";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		template.update(sql, param);
+	}
 
 }
