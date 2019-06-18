36846769333ded288a6af55186c711195c08534f
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-18 15:55:44 +0900

    コメントクラスの作成

---
+++ src/main/java/com/example/domain/Comment.java
@@ -0,0 +1,49 @@
+package com.example.domain;
+
+public class Comment {
+	/** コメントID*/
+	private Integer id;
+	/** コメント者名*/
+	private String name;
+	/** コメント内容*/
+	private String content;
+	/** 投稿ID*/
+	private Integer articleId;
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", name=" + name + ", content=" + content + ", articleId=" + articleId + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+}
