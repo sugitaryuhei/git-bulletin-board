66a60f7788073d1d6a83b1dfbac1bfaa61cb06dc
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-18 17:23:25 +0900

    リポジトリ修正

--- sql/test.sql
+++ sql/test.sql
@@ -1,7 +1,9 @@
-create table users_gitsample(
-id serial NOT NULL,
-name text NOT NULL,
-mail_address text NOT NULL,
-password text NOT NULL
-);
-insert into users_gitsample(name,mail_address,password)values('admin','admin','admin');
+-- create table users_gitsample(
+-- id serial NOT NULL,
+-- name text NOT NULL,
+-- mail_address text NOT NULL,
+-- password text NOT NULL
+-- );
+-- insert into users_gitsample(name,mail_address,password)values('admin','admin','admin');
+
+select mail_address,name,password from users_gitsample where mail_address='admin';
