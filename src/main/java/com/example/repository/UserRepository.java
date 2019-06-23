da089c449e981c77253062de08d98527911f292f
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-06-24 00:23:19 +0900

    world

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -15,9 +15,6 @@ public class UserRepository {
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
-	
-	
-	
 	private static final RowMapper<User> USER_ROW_MAPPER =(rs,i)->{
 		User user = new User();
 		user.setMailAddress(rs.getString("mail_address"));
